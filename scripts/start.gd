47f1a44c69203862918ce431fa1eb1a3fd8560cd
penzboti <penzboti@gmail.com>
2026-03-03 23:46:04 +0100

    reorganized n removed some files, disabled leaderb

--- scripts/start.gd
+++ scripts/start.gd
@@ -12,5 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	MusicManager2.play_music()
+	# MusicManager2.play_music() # should start on game-load
 	get_tree().change_scene_to_file("res://scenes/Level0.tscn")
